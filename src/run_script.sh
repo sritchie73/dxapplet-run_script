111abec4faa83ce124ae51ea0178d0482d778e23
Dr. Scott C. Ritchie <sr827@medschl.cam.ac.uk>
2026-02-12 14:18:53 +0000

    Permissions

--- src/run_script.sh
+++ src/run_script.sh
@@ -31,7 +31,7 @@ main() {
 
     # Create user library - remotes::install_github won't do this unlike install.packages
     Rscript -e 'system(sprintf("mkdir -p R/x86_64-pc-linux-gnu-library/%s.%s/", R.version$major, gsub("\\.[0-9]", "", R.version$minor)))'
-    chmod -R u+rwx R/
+    chmod -R 775 R/
 
     # Install 'dxutils' R package - we do this each time we load instead of
     # prebundling because the package is very much in beta, likely to update,
